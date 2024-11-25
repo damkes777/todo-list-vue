049a9cf9c85f9d099eb52ccdbb4bd31890c9ec3a
Damian Kędra <dkedra@esus-it.com>
2024-11-25 19:56:35 +0100

    create and delete task

---
+++ src/components/ListItem.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  props: {
+    index: {
+      type: Number,
+      required: true
+    }
+  },
+  emits: ['deleteTask']
+}
+</script>
+<template>
+  <div>
+    <slot name="content"></slot>
+  </div>
+  <div class="buttons">
+    <button @click="$emit('deleteTask', index)">delete</button>
+  </div>
+</template>
